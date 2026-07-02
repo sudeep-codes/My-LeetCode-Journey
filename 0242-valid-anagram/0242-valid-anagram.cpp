01bfc99b6825215c9f7f0094b1c6a363d54111ef
sudeep-codes <work.sudeep04@gmail.com>
2026-07-02 07:48:41 +0530

    Time: 2 ms (50.96%), Space: 9.7 MB (76.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,8 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        vector<int> freq(26,0);
+        for(int i=0; i<s.size(); i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int x:freq){
+            if(x!=0) return false;
+        }
+        return true;
     }
 };
